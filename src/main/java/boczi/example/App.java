e3cc32a3f389af3e3976e4607850adfa163fc7cf
Piotr Boczar <boczarpiotr@gmail.com>
2021-04-21 10:15:30 +0200

    changed POM for packing to jar to be possible.

--- src/main/java/boczi/example/App.java
+++ src/main/java/boczi/example/App.java
@@ -14,16 +14,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Define file type
-        //Run songFileReader
-        //
-        // fori po args czy csv czy xml
-
         SongCSVFileReader songCSVFileReader = new SongCSVFileReader();
         SongXMLFileReader songXMLFileReader = new SongXMLFileReader();
-        List<Song> list = songCSVFileReader.read(args[0]);
-        List<Song> list2 = songXMLFileReader.read(args[1]);
-        InMemorySongRepository inMemorySongRepository = new InMemorySongRepository();
+
+        System.out.println("hello world");
+
+
+   //     List<Song> list =  songCSVFileReader.read(args[0]);
+//        List<Song> list2 = songXMLFileReader.read(args[1]);
+//        InMemorySongRepository inMemorySongRepository = new InMemorySongRepository();
 //        list.forEach((song) -> {inMemorySongRepository.addSong(song);});
 //        list2.forEach(song -> inMemorySongRepository.addSong(song));
     }
